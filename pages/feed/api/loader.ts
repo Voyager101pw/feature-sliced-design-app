7ed49a4238b98a6a3a762f31935bdfe20d0b72b4
voyager101pw <voyager101sirius@gmail.com>
2024-05-06 05:26:33 +0500

    Feature: filtering by tags
    https://feature-sliced.design/ru/docs/get-started/tutorial#%D1%84%D0%B8%D0%BB%D1%8C%D1%82%D1%80%D0%B0%D1%86%D0%B8%D1%8F-%D0%BF%D0%BE-%D1%82%D0%B5%D0%B3%D0%B0%D0%BC

--- pages/feed/api/loader.ts
+++ pages/feed/api/loader.ts
@@ -1,13 +1,30 @@
-import { json } from "@remix-run/node";
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import type { FetchResponse } from "openapi-fetch";
+import { promiseHash } from "remix-utils/promise";
 
 import { GET } from "shared/api";
 
-export const loader = async () => {
-  const { data: articles, error, response } = await GET("/articles");
+async function throwAnyErrors<T, O, Media extends `${string}/${string}`>(
+  responsePromise: Promise<FetchResponse<T, O, Media>>,
+) {
+  const { data, error, response } = await responsePromise;
 
   if (error !== undefined) {
     throw json(error, { status: response.status });
   }
 
-  return json({ articles });
+  return data as NonNullable<typeof data>;
+}
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const selectedTag = url.searchParams.get("tag") ?? undefined;
+
+  return json(
+    await promiseHash({
+      articles: throwAnyErrors(
+        GET("/articles", { params: { query: { tag: selectedTag } } }),
+      ),
+      tags: throwAnyErrors(GET("/tags")),
+    }),
+  );
 };
