8429c69842ce16f4c85d1dd0a11ed3eb5b40299b
voyager101pw <voyager101sirius@gmail.com>
2024-05-06 06:25:38 +0500

    Feature: Created register & signin pages
    https://feature-sliced.design/ru/docs/get-started/tutorial#%D0%B0%D1%83%D1%82%D0%B5%D0%BD%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%86%D0%B8%D1%8F

---
+++ pages/sign-in/index.ts
@@ -0,0 +1,5 @@
+export { RegisterPage } from "./ui/RegisterPage";
+export { register } from "./api/register";
+
+export { SignInPage } from "./ui/SignInPage";
+export { signIn } from "./api/sign-in";
