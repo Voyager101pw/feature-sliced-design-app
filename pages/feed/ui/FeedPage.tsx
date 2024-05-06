367f4b7e25d2320f2d3a705ecd2dc323a11c626f
voyager101pw <voyager101sirius@gmail.com>
2024-05-06 05:10:58 +0500

    Display feed on main page
    https://feature-sliced.design/ru/docs/get-started/tutorial#%D1%80%D0%B5%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5-%D0%B2-%D0%BB%D0%B5%D0%BD%D1%82%D0%B5

--- pages/feed/ui/FeedPage.tsx
+++ pages/feed/ui/FeedPage.tsx
@@ -1,4 +1,11 @@
-const FeedPage = () => {
+import { useLoaderData } from "@remix-run/react";
+
+import type { loader } from "../api/loader";
+import { ArticlePreview } from "./ArticlePreview";
+
+export function FeedPage() {
+  const { articles } = useLoaderData<typeof loader>();
+
   return (
     <div className="home-page">
       <div className="banner">
@@ -7,8 +14,16 @@ const FeedPage = () => {
           <p>A place to share your knowledge.</p>
         </div>
       </div>
+
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            {articles.articles.map((article) => (
+              <ArticlePreview key={article.slug} article={article} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
-
-export { FeedPage };
+}
